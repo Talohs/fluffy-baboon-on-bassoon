acaf8baa9c6bd2cb00e546c57bfe72f7364139b2
Talohs <blackholesoul@gmail.com>
2022-12-10 20:40:45 -0500

    working full

--- cart.py
+++ cart.py
@@ -10,13 +10,13 @@
     #if statements to process the shopping cart responses
 # Have a print statement at the end that prints their reicept in a formatted way
 
-shopping_cart = []
+shopping_cart = ['bananas', {'number of bananas': '5', 'cost of bananas': '6.92'}, 'apples', {'number of apples': '3', 'cost of apples': '6.13'}, 'pears', {'number of pears': '10', 'cost of pears': '5.07'}]
 hold_cart = {}
+total_cost = 18.12
 def user_resp(u_choice):
-    print(u_choice)
     if u_choice not in ['add', 'delete', 'clear', 'show', 'quit']:
         print("Choice selection are add, delete, clear, show, or quit")
-        u_choice = input("What would you like to do in the store today?").lower()
+        u_choice = input("What would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower()
         user_resp(u_choice)
     user_choices = {
         'add': 'add',
@@ -28,59 +28,67 @@ def user_resp(u_choice):
     return user_choices.get(u_choice)
 
 def item_cost(item, count):
+    global total_cost
     cost_numeric = 0.0
     for cost_item in item:
         cost_numeric += ord(cost_item) * 0.010
     cost_numeric*float(count)
     cost_total = str("%.2f" % cost_numeric)
-    # shopping_cart.update({f'{item} price': cost_total})
+    total_cost += cost_numeric
     return cost_total
 
 def choice_add():
     add_cart = {}
-    new_item = input('Please add a food?')
+    new_item = input('What would you like to buy?')
     shopping_cart.append(new_item)
     item_count = input (f"How many {new_item} would you like?")
     add_cart.update({f'number of {new_item}': item_count})
     item_total = item_cost(new_item, item_count)
     add_cart.update({f'cost of {new_item}': item_total})
     shopping_cart.append(add_cart)
-    print(shopping_cart)
-    # return add_cart
 
-# def choice_del():
-#     rid_me = input("What item would you like to remove?")
-#     if rid_me in shopping_cart:
-#         shopping_cart[shopping_cart.index(rid_me):shopping_cart.index(rid_me)+2]
-#     else:
-#         print("You haven't bought any of those yet")
+
+def choice_del():
+    global total_cost
+    global shopping_cart
+    rid_me = input("What item would you like to remove?").lower()
+    if rid_me in shopping_cart:
+        total_cost -= float(shopping_cart[shopping_cart.index(rid_me)+1].get(f'cost of {rid_me}'))
+        shopping_cart = shopping_cart[:shopping_cart.index(rid_me)] + shopping_cart[shopping_cart.index(rid_me)+2:]
+    else:
+        print("You haven't bought any of those yet")
+
+def print_out():
+    for ct,val in enumerate(shopping_cart):
+        if isinstance(val, str):
+            item = val
+            nums = [num for num in shopping_cart[ct+1].values()]
+            print(f"{nums[0]} {item.title()} for ${nums[1]}")
+    print(f'${"%.2f" % total_cost}')
+
     
 def user_attr():
-    c_choice = user_resp(input("What would you like to do in the store today?").lower())
+    global total_cost
+    c_choice = user_resp(input("What would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower())
     while c_choice != 'quit':
         if c_choice == 'add':
             choice_add()
-            print(shopping_cart)
-            # hold_cart.update(choice_add(hold_cart).copy())
-            # print(choice_add(hold_cart).copy())
-            c_choice = user_resp(input("What else would you like to do in the store today?"))
+            c_choice = user_resp(input("What else would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower())
         elif c_choice == 'clear':
             shopping_cart.clear()
-        elif c_choice == 'delete'
-            # choice_del()        
+            total_cost = 0.00
+            c_choice = user_resp(input("What else would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower())
+        elif c_choice == 'delete':
+            choice_del()
+            c_choice = user_resp(input("What else would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower())
+        elif c_choice == 'show':
+            print_out()
+            c_choice = user_resp(input("What else would you like to do in the store today? Choice selection are add, delete, clear, show, or quit").lower())
     return hold_cart
     
-# print(user_resp())
-# user_resp(input("What would you like to do in the store today?"))
 user_attr()
-for ct,val in enumerate(shopping_cart):
-    if isinstance(val, str):
-        item = val
-        nums = [num for num in shopping_cart[ct+1].values()]
-        print(f"{nums[0]} {item} for ${nums[1]}")
+print("Your Reciept")
+print_out()
 
-        
-    # print(ct, val,)
-    # print("Your reciept\n", f"{val[ct+1].get(f'number of {val}')} {val} for ${val[ct+1].get(f'cost of {val}')}\n")
 
-print(shopping_cart)
+
